a30e14b894f69531dec5a60b514c2fb4474dcd21
kaushal16124 <kaushalkishor0000@gmail.com>
2024-07-17 19:08:50 +0530

    Added Image above footer

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import bgVideo from '../assets/BGhome.mp4'
 import bgVideoMobile from '../assets/homeMobBG.mp4'
 import Footer from './Footer';
+import imageWingedUp from '../assets/wingedUpImage.png'
 
 const Home = (props) => {
   const { setProgress } = props;
@@ -16,10 +17,12 @@ const Home = (props) => {
   return (
     <>
       <div>
+      
         <video autoPlay loop muted className="bg-vid" disablePictureInPicture>
         <source src={bgVideo} type="video/mp4" media="screen and (min-width: 768px)" />
         <source src={bgVideoMobile} type="video/mp4" media="screen and (max-width: 767px)" />
         </video>
+        <img src={imageWingedUp} alt="WingedUp Image" style={{ width: '100%', height: 'auto', objectFit: 'cover' }}/>
       </div>
       {/* <div className="get-wings-now">
         <h1><strong>Get your wings now!</strong></h1>
